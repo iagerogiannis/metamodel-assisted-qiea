4cf6373d7c9a7db50a2dafd803e57ab972ce8d7f
Giannis Gerogiannis <iagerogiannis@gmail.com>
2026-03-08 20:34:54 +0200

    Remove redundant files

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,5 +3,4 @@ from .list_operations import *
 from .string_operations import *
 from .visualization import *
 from .datapoints import *
-from .easy_config_generator import *
 from .case_studies import *
